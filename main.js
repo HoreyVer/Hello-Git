fc9c6de047813c8152289946cdd5dfc3c7d43d81
HoreyVer <artjom.bodroff@yandex.ru>
2022-09-29 17:08:01 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function multByFactor(value, multiplier = 2) {
+	return value * multiplier;
+};
+
+let u = (value, multiplier) => value * multiplier;
+
+(function (value = 10, multiplier = 2) {
+	console.log(value * multiplier);
+})();
